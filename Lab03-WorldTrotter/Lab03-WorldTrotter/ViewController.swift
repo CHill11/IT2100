5f116d1299c7012898de7ecff154f11deaab3084
Christopher Hill <papermario2@sbcglobal.net>
2022-02-02 02:16:44 -0500

    Lab 03 WorldTrotter
    Lab 03 WorldTrotter

---
+++ Lab03-WorldTrotter/Lab03-WorldTrotter/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Lab03-WorldTrotter
+//
+//  Created by Christopher Hill on 2/1/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    let gradient = CAGradientLayer()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        gradient.colors = [UIColor.purple.cgColor,
+                           UIColor.red.cgColor,
+                           UIColor.green.cgColor,
+                           UIColor.blue.cgColor]
+        gradient.frame = view.bounds
+        view.layer.insertSublayer(gradient, at: 0)
+        }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        
+        gradient.frame = view.bounds
+    }
+}
+

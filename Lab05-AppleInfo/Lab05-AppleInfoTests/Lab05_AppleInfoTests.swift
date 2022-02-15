277b52b83fa100acf800b0b72284e0dc3c1604d0
CHill11 <77762021+CHill11@users.noreply.github.com>
2022-02-15 09:18:20 -0500

    Added Lab 5

--- Lab05-AppleInfo/Lab05-AppleInfoTests/Lab05_AppleInfoTests.swift
+++ Lab05-AppleInfo/Lab05-AppleInfoTests/Lab05_AppleInfoTests.swift
@@ -30,7 +30,7 @@ class Lab05_AppleInfoTests: XCTestCase {
         // This is an example of a performance test case.
         self.measure {
             // Put the code you want to measure the time of here.
-        }
+        } 
     }
 
 }

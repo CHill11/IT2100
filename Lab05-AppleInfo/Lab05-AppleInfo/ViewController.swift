11e6f715b5d9f20c7b85d4c48a6419e79d1bd9ea
CHill11 <77762021+CHill11@users.noreply.github.com>
2022-02-15 09:23:56 -0500

    Week 5

--- Lab05-AppleInfo/Lab05-AppleInfo/ViewController.swift
+++ Lab05-AppleInfo/Lab05-AppleInfo/ViewController.swift
@@ -6,14 +6,39 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var appleLink: UISegmentedControl!
+    @IBOutlet var webApple: WKWebView!
+    
+    @IBAction func changeAppleLink(_sender:Any) {
+        var appleURLString = ""
+        switch appleLink.selectedSegmentIndex {
+        case 0:
+            appleURLString = "https://en.wikipedia.org/wiki/Apple_Inc."
+        case 1:
+            appleURLString = "https://en.wikipedia.org/wiki/IOS"
+        case 2:
+            appleURLString = "https://en.wikipedia.org/wiki/Xcode"
+        case 3:
+            appleURLString = "https://en.wikipedia.org/wiki/Swift_(programming_language)"
+        default:
+            break
+        }
+        let myURL = URL(string: appleURLString)
+        let myRequest = URLRequest(url: myURL!)
+        webApple.load(myRequest)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let myURL = URL(string: "https://en.wikipedia.org/wiki/Apple_Inc.")
+        let myRequest = URLRequest(url: myURL!)
+        webApple.load(myRequest)
     }
-
 
 }
 

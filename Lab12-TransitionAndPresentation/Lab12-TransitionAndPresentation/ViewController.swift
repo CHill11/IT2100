7493f974b037098dd97a1c6e7f747c8e682001be
CHill11 <77762021+CHill11@users.noreply.github.com>
2022-04-11 20:46:10 -0400

    Competed Lab 12

---
+++ Lab12-TransitionAndPresentation/Lab12-TransitionAndPresentation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Lab12-TransitionAndPresentation
+//
+//  Created by Christopher Hill on 4/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func changeView(_ sender: UIButton) {
+        let newView = SecondViewController()
+        newView.modalPresentationStyle = .fullScreen
+        newView.modalTransitionStyle = .flipHorizontal
+        present(newView, animated: true)
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Lab12-TransitionAndPresentation/Lab12-TransitionAndPresentationUITests/Lab12_TransitionAndPresentationUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Lab12_TransitionAndPresentationUITestsLaunchTests.swift
+//  Lab12-TransitionAndPresentationUITests
+//
+//  Created by Christopher Hill on 4/11/22.
+//
+
+import XCTest
+
+class Lab12_TransitionAndPresentationUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
